db6d85f4ab02f1a63d1871682515167fe144cd34
leonardi <gino.leonardi@euris.it>
2024-04-12 12:41:27 +0200

    method for duplicates

--- ConsoleAppComuni/ClassLibraryComuni/Stat.cs
+++ ConsoleAppComuni/ClassLibraryComuni/Stat.cs
@@ -1,4 +1,6 @@
-﻿using ClassLibraryComuni.FileHelper;
+﻿using System.Collections.Generic;
+using System.IO;
+using ClassLibraryComuni.FileHelper;
 
 namespace ClassLibraryComuni
 {
@@ -14,10 +16,34 @@ namespace ClassLibraryComuni
 
             foreach (var item in context)
             {
-                regione = item.Value.Count > val ? item.Key : regione;
+                if (item.Value.Count > val)
+                {
+                    regione = item.Key;
+                    val = item.Value.Count;
+                }
             }
 
+         //var regione =  context.MaxBy(pair => pair.Value.Count).Key;
+
             return regione;
+        }
+
+        public void Duplicate(string path)
+        {
+            using var data = new Data(path);
+            data.Initialize();
+            var context = data.GetAll();
+            var myList = new Dictionary<string, int>();
+        
+
+            foreach (var item in context)
+            {
+                if (myList.ContainsKey(item.Key))
+                    myList[item.Key]++;
+                else myList.Add(item.Key,1);
+
+            }
+            
         }
     }
 }
